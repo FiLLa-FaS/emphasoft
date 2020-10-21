e05a12238561ed3d9285d1114dc232bcb5325472
FiLLa-FaS <fillafas@yandex.ru>
2020-10-21 16:09:46 +0300

    Updates

---
+++ precache-manifest.db1b73814e9d399b14c69e05d129c874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9127932986e4b9aceb25b6a8b87e9e",
+    "url": "/emphasoft/index.html"
+  },
+  {
+    "revision": "91740510b07ca41795f7",
+    "url": "/emphasoft/static/js/2.1bf6c605.chunk.js"
+  },
+  {
+    "revision": "e66108cc5333ee91f3c058a031032d08",
+    "url": "/emphasoft/static/js/2.1bf6c605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed61b1fbd5806065587",
+    "url": "/emphasoft/static/js/main.03fdc61b.chunk.js"
+  },
+  {
+    "revision": "7c57b725d8957e80a18d",
+    "url": "/emphasoft/static/js/runtime-main.81be181a.js"
+  }
+]);
